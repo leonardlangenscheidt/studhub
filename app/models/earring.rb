1738962eeda5d291109e7706076f6bf191d34600
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-07-26 13:49:36 -0400

    implementing s3 for production uploads of pictures

--- app/models/earring.rb
+++ app/models/earring.rb
@@ -1,8 +1,10 @@
 class Earring < ActiveRecord::Base
 	validates :brand, :color, :user_id, presence: true
 
-      validates :image, presence:true
+      	validates :image, presence:true
 
 	belongs_to :user
-      has_attached_file :image, styles: { medium: "320x240>"}
+      	has_attached_file :image, styles: {
+      		medium: "320x240>"
+      	}
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,87 +1,97 @@
 Omrails::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+      # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
-  config.cache_classes = true
+      #paperclip in heroku settings
+    config.paperclip_defaults = {
+        :storage => :s3,
+        :s3_credentials => {
+            :bucket => ENV['AWS_BUCKET'],
+            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+            }
+        }
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both thread web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+      # Code is not reloaded between requests.
+      config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+      # Eager load code on boot. This eager loads most of Rails and
+      # your application in memory, allowing both thread web servers
+      # and those relying on copy on write to perform better.
+      # Rake tasks automatically ignore this option for performance.
+      config.eager_load = true
 
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
+      # Full error reports are disabled and caching is turned on.
+      config.consider_all_requests_local       = false
+      config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+      # Enable Rack::Cache to put a simple HTTP cache in front of your application
+      # Add `rack-cache` to your Gemfile before enabling this.
+      # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+      # config.action_dispatch.rack_cache = true
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+      # Disable Rails's static asset server (Apache or nginx will already do this).
+      config.serve_static_assets = false
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  #changed to true due to stackoverflow.com/questions/9572292/heroku-not-displaying-finding-css-and-js-files
-  config.assets.compile = true
+      # Compress JavaScripts and CSS.
+      config.assets.js_compressor = :uglifier
+      # config.assets.css_compressor = :sass
 
-  # Generate digests for assets URLs.
-  config.assets.digest = true
+      # Do not fallback to assets pipeline if a precompiled asset is missed.
+      #changed to true due to stackoverflow.com/questions/9572292/heroku-not-displaying-finding-css-and-js-files
+      config.assets.compile = true
 
-  # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+      # Generate digests for assets URLs.
+      config.assets.digest = true
 
-  # adding from stackoverflow.com/questions/9572292/heroku-not-displaying-finding-css-and-js-files
-  config.assets.enabled = false
+      # Version of your assets, change this if you want to expire all your assets.
+      config.assets.version = '1.0'
 
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+      # adding from stackoverflow.com/questions/9572292/heroku-not-displaying-finding-css-and-js-files
+      config.assets.enabled = false
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+      # Specifies the header that your server uses for sending files.
+      # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+      # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
-  # Set to :debug to see everything in the log.
-  config.log_level = :info
+      # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+      # config.force_ssl = true
 
-  # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
+      # Set to :debug to see everything in the log.
+      config.log_level = :info
 
-  # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+      # Prepend all log lines with the following tags.
+      # config.log_tags = [ :subdomain, :uuid ]
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+      # Use a different logger for distributed setups.
+      # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+      # Use a different cache store in production.
+      # config.cache_store = :mem_cache_store
 
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w[ bootstrap/*.css bootstrap/*.js ]
+      # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+      # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+      # Precompile additional assets.
+      # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+      config.assets.precompile += %w[ bootstrap/*.css bootstrap/*.js ]
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found).
-  config.i18n.fallbacks = true
+      # Ignore bad email addresses and do not raise email delivery errors.
+      # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+      # config.action_mailer.raise_delivery_errors = false
 
-  # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+      # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+      # the I18n.default_locale when a translation can not be found).
+      config.i18n.fallbacks = true
 
-  # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
+      # Send deprecation notices to registered listeners.
+      config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+      # Disable automatic flushing of the log to improve performance.
+      # config.autoflush_log = false
 
-  #:host should be set to the actual host of your application.
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+      # Use default logging formatter so that PID and timestamp are not suppressed.
+      config.log_formatter = ::Logger::Formatter.new
+
+      #:host should be set to the actual host of your application.
+      config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
