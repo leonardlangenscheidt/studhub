6195380d1240e10b0ea6d42088114b63bf78cfe1
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-07-29 10:45:24 -0400

    reverting some changes in model

--- app/models/earring.rb
+++ app/models/earring.rb
@@ -5,8 +5,8 @@ class Earring < ActiveRecord::Base
 
 	belongs_to :user
       	has_attached_file :image,
-	path: ":rails_root/public/system/:attachment/:id/:style/:filename",
-       	url: "/system/:attachment/:id/:style/:filename"
+	# path: ":rails_root/public/system/:attachment/:id/:style/:filename",
+ #       	url: "/system/:attachment/:id/:style/:filename"
       	styles: {
       		medium: "320x240>"
       	}
