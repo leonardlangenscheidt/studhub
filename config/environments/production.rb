17b541403394e2528244148d28c8b5d8a7339561
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-07-29 09:33:30 -0400

    new aws secret access key and changes in heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,9 +5,9 @@ Omrails::Application.configure do
     config.paperclip_defaults = {
         :storage => :s3,
         :s3_credentials => {
-            :bucket => ENV['AWS_BUCKET'],
-            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+            :bucket => ENV['studhubearringimage'],
+            :access_key_id => ENV['AKIAI4LXISC7FMRHT2YQ'],
+            :secret_access_key => ENV['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']
             }
         }
 
