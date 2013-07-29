08fd5b8b2e0321344a43c11bf9ddc196e5a56aba
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-07-29 10:39:56 -0400

    more and more

--- app/models/earring.rb
+++ app/models/earring.rb
@@ -4,7 +4,10 @@ class Earring < ActiveRecord::Base
       	validates :image, presence:true
 
 	belongs_to :user
-      	has_attached_file :image, styles: {
+      	has_attached_file :image,
+	path: ":rails_root/public/system/:attachment/:id/:style/:filename",
+       	url: "/system/:attachment/:id/:style/:filename"
+      	styles: {
       		medium: "320x240>"
       	}
 end
